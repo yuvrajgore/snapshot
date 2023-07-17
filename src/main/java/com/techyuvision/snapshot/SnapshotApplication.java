143cec68865d942894925ed59c06b7e3fc7a20cf
yuvraj <yuvrajgore123@gmail.com>
2023-07-17 17:38:05 +0530

    first commit

---
+++ src/main/java/com/techyuvision/snapshot/SnapshotApplication.java
@@ -0,0 +1,25 @@
+package com.techyuvision.snapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapshotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapshotApplication.class, args);
+		System.out.println("Hello World..");
+		List<String> argList = new ArrayList<>();
+		argList.add("String A");
+		argList.add("String B");
+		argList.add("String C");
+		for (String str : argList) {
+			System.out.println(str);
+		}
+		System.out.println("Length of List: "+ argList.size());
+	}
+
+}
